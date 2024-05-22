e553371827f96f7d63a0f561abb754e1d7647082
leo.gerard <leo.gerard@GERARDL-N>
2024-05-22 14:24:26 +0200

    Ajout de entity + context (mais marche pas erreur de bdd)

---
+++ Dofus_HDV/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Dofus_HDV.Data;
+
+namespace Dofus_HDV.Controllers 
+{
+    public class ItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var items = _context.Items.ToList();
+            return Ok(items);
+        }
+    }
+}
--- Dofus_HDV/Data/Item.cs
+++ Dofus_HDV/Data/Item.cs
@@ -1,21 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Dofus_HDV.Data
 {
     public class Item
     {
+        [Key]
+        [Column("ItemID")]
         public int ItemID { get; set; }
+
+        [Column("ItemName")]
         public string ItemName { get; set; }
+
+        [Column("CategoryID")]
         public int CategoryID { get; set; }
+
+        [Column("Description")]
         public string Description { get; set; }
+
+        [Column("Price")]
         public decimal Price { get; set; }
+
+        [Column("Quantity")]
         public int Quantity { get; set; }
+
+        [Column("CreatedAt")]
         public DateTime CreatedAt { get; set; }
 
-        public Item()
-        {
-            // Default constructor
-        }
+
+        public Item() { }
 
         public Item(string itemName, int categoryId, string description, decimal price, int quantity)
         {
